8819f3ac06fb0535a95ee4b64714fcdc9fed70dc
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2022-03-26 12:41:53 +0100

    fix code

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const { translate } = require('free-translate');
 const fs = require('fs')
 
 const readStream = fs.createReadStream('./input.txt')
+const writeFile = (filename, translateTo) => {
+    new Promise((resolve, reject) => {
+        fs.writeFile(filename, translateTo, (err, data) => {
+            if (err) {
+                return reject(err)
+            }
+            resolve(data)
+        })
+    })
+}
 
 const asyncIterator = async (readeble) => {
     for await (const chank of readeble) {
@@ -9,18 +19,8 @@ const asyncIterator = async (readeble) => {
         const translateTextToEnglish = await translate(chank, { from: 'ru', to: 'en' })
         const translateTextToJapanse = await translate(chank, { from: 'ru', to: 'zh-CN' })
         console.log(translateTextToEnglish)
-        const writeFile = (filename, translateTo) => {
-            new Promise((resolve, reject) => {
-                fs.writeFile(filename, translateTo, (err, data) => {
-                    if (err) {
-                        return reject(err)
-                    }
-                    resolve(data)
-                })
-            })
-        }
-        writeFile('en.translate.txt', translateTextToEnglish)
-        writeFile('cn.translate.txt', translateTextToJapanse)
+        await writeFile('en.translate.txt', translateTextToEnglish)
+        await writeFile('cn.translate.txt', translateTextToJapanse)
     }
 }
 
